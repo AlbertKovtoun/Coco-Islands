67e494c67ddabe1363295ad284fd25580ae65195
AlbertKovtoun <kovtoun.albert@hotmail.com>
2021-11-12 15:53:18 +0100

    Test for gltfLoader

--- src/javascript/three/Wind.js
+++ src/javascript/three/Wind.js
@@ -1,20 +1,28 @@
 import * as THREE from "three"
+import gsap from "gsap"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 import { pane, scene } from "./Experience"
 
 import windVertexShader from "../../shaders/wind/vertex.glsl"
 import windFragmentShader from "../../shaders/wind/fragment.glsl"
 
-const windMeshPath = "/assets/models/Wind1.gltf"
+const windMeshPath1 = "/assets/models/Wind1.gltf"
+const windMeshPath2 = "/assets/models/Wind2.gltf"
 
 export class Wind {
   constructor() {
     this.gltfLoader = new GLTFLoader()
 
+    this.wind1
+    this.wind2
     this.windMaterial
 
     this.setWind()
-    this.setWindTweaks()
+
+    setTimeout(() => {
+      this.setWindTweaks()
+      this.setWindAnimation()
+    }, 100)
   }
 
   setWind() {
@@ -28,24 +36,41 @@ export class Wind {
       },
     })
 
-    this.gltfLoader.load(windMeshPath, (gltf) => {
-      this.wind = gltf.scene
+    //Load Wind1
+    this.gltfLoader.load(windMeshPath1, (gltf) => {
+      this.wind1 = gltf.scene
 
-      this.wind.traverse((child) => {
+      this.wind1.traverse((child) => {
         if (child.isMesh) child.material = this.windMaterial
       })
 
-      this.wind.position.set(0, 0.5, 0)
-      scene.add(this.wind)
+      this.wind1.position.set(0, 0.5, 0)
+      scene.add(this.wind1)
+    })
+
+    //Load wind2
+    this.gltfLoader.load(windMeshPath2, (gltf) => {
+      this.wind2 = gltf.scene
+
+      this.wind2.traverse((child) => {
+        if (child.isMesh) child.material = this.windMaterial
+      })
+
+      this.wind2.position.set(0.5, 1, -1)
+      scene.add(this.wind2)
     })
   }
 
   setWindTweaks() {
-    pane.addInput(this.windMaterial.uniforms.uProgress, "value", {
-      min: 0,
-      max: 1,
-      step: 0.001,
-      label: "windProgress",
-    })
+    // pane.addInput(this.windMaterial.uniforms.uProgress, "value", {
+    //   min: 0,
+    //   max: 1,
+    //   step: 0.001,
+    //   label: "windProgress",
+    // })
+  }
+
+  setWindAnimation() {
+    console.log(this.wind1)
   }
 }
--- src/shaders/wind/fragment.glsl
+++ src/shaders/wind/fragment.glsl
@@ -4,7 +4,7 @@ varying vec2 vUv;
 
 void main()
 {
-    float progressAlpha = (1.0 - abs(uProgress - vUv.x) * 10.0);
+    float progressAlpha = (1.0 - abs(uProgress - vUv.x) * 5.0);
 
     float endsAlpha = min(vUv.x * 10.0, (1.0 - vUv.x) * 10.0);
 
